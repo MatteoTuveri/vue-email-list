2c60580fe61a3be654c3f1b25361a77ea72bdb94
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-11-07 16:02:02 +0100

    Milestone 1 + Bonus

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,25 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            apiBody : 'https://flynn.boolean.careers/exercises/api',
+            endPoint : '',
+            mailList: [],
+
         }
+    },
+    methods: {
+        getData() {
+            this.endPoint = '/random/mail'
+            for (let i = 0; i < 10; i++) {
+                axios.get(`${this.apiBody}${this.endPoint}`).then((resp) => {
+                    console.log(resp)
+                    const result = resp.data.response;
+                    this.mailList.push(result);
+                })
+            }
+        }
+    },
+    mounted() {
+        this.getData()
     }
 }).mount('#app')
